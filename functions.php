1c99b0199d550aa8ba7cbb4e4c78d9382042f78c
karlkruu <karlkruu@greeny.cs.tlu.ee>
2016-11-08 12:11:51 +0200

    Brought files from 9th lesson (classes)

---
+++ functions.php
@@ -0,0 +1,28 @@
+<?php
+
+	require("../../config.php");
+	
+	
+	//ühendus
+	$database="if16_karlkruu";
+	$mysqli=new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+	
+	//klassid
+	require("User.class.php");
+	$User=new User($mysqli);
+	
+	require("Interest.class.php");
+	$Interest=new Interest($mysqli);
+	
+	require("Car.class.php");
+	$Car=new Car($mysqli);
+	
+	require("Helper.class.php");
+	$Helper=new Helper($mysqli);
+	
+	
+	//see fail peab olema kõigil lehtedel kus tahan kasutada SESSION muutujat
+	session_start();
+	
+
+?>
